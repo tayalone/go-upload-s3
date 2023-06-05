2c7836ac6f6c8edd3d1b001f3b2c89729f80b174
panupong rong-o <tayalone@gmail.com>
2023-06-05 09:29:43 +0700

    :heavy_plus_sign: add gin

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Start")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
