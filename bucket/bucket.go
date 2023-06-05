46d272c884ae793acc8f3cbcbf8cbdcfaaad3922
panupong rong-o <tayalone@gmail.com>
2023-06-05 12:39:47 +0700

    :sparkles: create bucket pkg

---
+++ bucket/bucket.go
@@ -0,0 +1,148 @@
+package bucket
+
+import (
+	"fmt"
+	"log"
+	"mime/multipart"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// UploadResp return from Upload File to bucket
+type UploadResp struct {
+	isError bool
+	err     string
+	key     string
+	url     string
+}
+
+// Bucket for Handler Static File
+type Bucket interface {
+	Healtz() error
+	Upload(file *multipart.FileHeader, prefix string) (UploadResp, error)
+	FileExist(key string) error
+	Remove(ket string) error
+}
+
+// Domain of Bucket
+type Domain struct {
+	region          string
+	accessKeyID     string
+	secretAccessKey string
+	bucketName      string
+	session         *session.Session // awsSession
+	client          *s3.S3           // ses3Client
+}
+
+// Initialize Bucket
+func Initialize(
+	region string,
+	accessKeyID string,
+	secretAccessKey string,
+	bucketName string,
+) Bucket {
+	awsSession, err := session.NewSession(&aws.Config{
+		Region:      aws.String(os.Getenv(region)),
+		Credentials: credentials.NewStaticCredentials(os.Getenv(accessKeyID), os.Getenv(secretAccessKey), ""),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	s3Client := s3.New(awsSession)
+
+	return &Domain{
+		region:          region,
+		accessKeyID:     accessKeyID,
+		secretAccessKey: secretAccessKey,
+		bucketName:      bucketName,
+		session:         awsSession,
+		client:          s3Client,
+	}
+}
+
+// getFullUrl of file
+func (d *Domain) getFullURL(key string) string {
+	return fmt.Sprintf("https://%s.s3-%s.amazonaws.com/%s", d.bucketName, d.region, key)
+}
+
+// Healtz of bucket
+func (d *Domain) Healtz() error {
+	_, err := d.client.ListBuckets(nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Upload of file to bucket
+func (d *Domain) Upload(file *multipart.FileHeader, prefix string) (UploadResp, error) {
+	src, err := file.Open()
+	if err != nil {
+		return UploadResp{
+			isError: true,
+			err:     err.Error(),
+		}, err
+	}
+	defer src.Close()
+	key := fmt.Sprintf("%s%s", prefix, file.Filename) // Add sub-folder path to the object key
+
+	params := &s3.PutObjectInput{
+		Bucket: aws.String(d.bucketName),
+		Key:    aws.String(key),
+		Body:   src,
+	}
+	_, err = d.client.PutObject(params)
+	if err != nil {
+		return UploadResp{
+			isError: true,
+			err:     err.Error(),
+		}, err
+	}
+
+	return UploadResp{
+		isError: false,
+		err:     "",
+		key:     key,
+		url:     d.getFullURL(key),
+	}, nil
+}
+
+// FileExist of file to bucket
+func (d *Domain) FileExist(key string) error {
+
+	// Check if the file exists in S3
+	headParams := &s3.HeadObjectInput{
+		Bucket: aws.String(d.bucketName),
+		Key:    aws.String(key),
+	}
+	_, err := d.client.HeadObject(headParams)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Remove of file to bucket
+func (d *Domain) Remove(key string) error {
+
+	err := d.FileExist(key)
+	if err != nil {
+		return err
+	}
+
+	// Remove the file from S3
+	deleteParams := &s3.DeleteObjectInput{
+		Bucket: aws.String(d.bucketName),
+		Key:    aws.String(key),
+	}
+	_, err = d.client.DeleteObject(deleteParams)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -4,11 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	awsSession, err := session.NewSession(&aws.Config{
+		Region:      aws.String(os.Getenv("AWS_S3_REGION")),
+		Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_S3_ACCESS_KEY_ID"), os.Getenv("AWS_S3_SECRET_ACCESS_KEY"), ""),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s3Client := s3.New(awsSession)
+
 	fmt.Println("Start")
 
 	r := gin.Default()
@@ -49,6 +69,7 @@ func main() {
 	/* Upload Multi files */
 	r.POST("/multi", func(c *gin.Context) {
 		form, _ := c.MultipartForm()
+		defer form.RemoveAll()
 		files := form.File["upload[]"]
 		for _, file := range files {
 			src, err := file.Open()
@@ -62,6 +83,72 @@ func main() {
 	})
 
 	/* ------------------- */
+
+	r.POST("/single-s3", func(c *gin.Context) {
+		// single file
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Bad request")
+			return
+		}
+		log.Println(file.Filename)
+
+		src, err := file.Open()
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Failed to open file")
+			return
+		}
+		defer src.Close()
+
+		/* do some logic */
+
+		key := fmt.Sprintf("test-go/%s", file.Filename) // Add sub-folder path to the object key
+
+		params := &s3.PutObjectInput{
+			Bucket: aws.String(os.Getenv("AWS_S3_BUCKET_NAME")),
+			Key:    aws.String(key),
+			Body:   src,
+		}
+
+		_, err = s3Client.PutObject(params)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Failed to upload file to S3")
+			return
+		}
+
+		// Check if the file exists in S3
+		headParams := &s3.HeadObjectInput{
+			Bucket: aws.String(os.Getenv("AWS_S3_BUCKET_NAME")),
+			Key:    aws.String(key),
+		}
+
+		_, err = s3Client.HeadObject(headParams)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok && aerr.Code() == "NotFound" {
+				c.String(http.StatusOK, fmt.Sprintf("'%s' does not exist in S3", file.Filename))
+				return
+			}
+			c.String(http.StatusInternalServerError, "Failed to check file existence in S3")
+			return
+		}
+
+		url := fmt.Sprintf("https://%s.s3-%s.amazonaws.com/%s", os.Getenv("AWS_S3_BUCKET_NAME"), os.Getenv("AWS_S3_REGION"), key)
+		/*  --------- */
+
+		// Remove the file from S3
+		deleteParams := &s3.DeleteObjectInput{
+			Bucket: aws.String(os.Getenv("AWS_S3_BUCKET_NAME")),
+			Key:    aws.String(key),
+		}
+
+		_, err = s3Client.DeleteObject(deleteParams)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Failed to remove file from S3")
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "OK", "url": url})
+	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
